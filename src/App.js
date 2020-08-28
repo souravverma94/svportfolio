14552b7b2e6892932356b03e08a60dc1c1a3d655
Sourav Verma <sourav.verma@ttu.edu>
2020-08-28 10:00:40 -0500

    minor update in portfolio style

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>My Portfolio</h1>
-      <h2>Author: Sourav Verma</h2>
-      <img style={{width: "300px"}} src={profileImage} alt="display" />
+      <header>
+        <img src={profileImage} alt="display" />
+        <h1>Hi, I am Sourav Verma, I am 26 years old, living in Lubbock, Texas</h1>
+        <h3>I am learning full stack development. Watch this space for my upcoming projects</h3>
+      </header>
     </div>
   );
 }
