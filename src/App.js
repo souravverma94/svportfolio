b51c581e871ee9b0b1411565e8bbd1e59323551c
Sourav Verma <sourav.verma@ttu.edu>
2020-08-27 23:22:16 -0500

    portfolio foundation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import profileImage from './img/sv.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My React Portfolio</h1>
+      <h2>Author: Sourav Verma</h2>
+      <img style={{width: "300px"}} src={profileImage} alt="display" />
     </div>
   );
 }
