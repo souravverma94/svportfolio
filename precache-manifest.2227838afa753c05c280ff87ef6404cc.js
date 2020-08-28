93e0eadc943dc2ec028344729284e72cc1dc1da3
Sourav Verma <sourav.verma@ttu.edu>
2020-08-27 23:23:18 -0500

    Updates

---
+++ precache-manifest.2227838afa753c05c280ff87ef6404cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a89824d78a551f17cf0a1c04d853f23",
+    "url": "/svportfolio/index.html"
+  },
+  {
+    "revision": "ed913327142083d679f7",
+    "url": "/svportfolio/static/css/main.b5fce335.chunk.css"
+  },
+  {
+    "revision": "d9c11bc1623df283eb96",
+    "url": "/svportfolio/static/js/2.0cbad202.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/svportfolio/static/js/2.0cbad202.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed913327142083d679f7",
+    "url": "/svportfolio/static/js/main.f660bd37.chunk.js"
+  },
+  {
+    "revision": "92b763a362c70313796d",
+    "url": "/svportfolio/static/js/runtime-main.9f1a66a5.js"
+  },
+  {
+    "revision": "e4562f239c64cb7b95cfbff939b6ac13",
+    "url": "/svportfolio/static/media/sv.e4562f23.png"
+  }
+]);
