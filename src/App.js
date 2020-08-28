ca4fbbc89101b7d2b09051d4ed9b84ecb329b1ea
Sourav Verma <sourav.verma@ttu.edu>
2020-08-27 23:35:24 -0500

    updated text

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>My React Portfolio</h1>
+      <h1>My Portfolio</h1>
       <h2>Author: Sourav Verma</h2>
       <img style={{width: "300px"}} src={profileImage} alt="display" />
     </div>
