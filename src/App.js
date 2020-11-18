877efbbb128c5cf6a2f2b53c85134824ebfee57a
Sourav Verma <sourav.verma@ttu.edu>
2020-11-17 16:48:25 -0800

    refactorized the code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import profileImage from './img/sv.png';
-import './App.css';
+import React from "react";
+import profileImage from "./img/sv.png";
+import { NavBar } from "./components/NavBar";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header>
-        <img src={profileImage} alt="display" />
-        <h1>Hi, I am Sourav Verma, I am 26 years old, living in Lubbock, Texas</h1>
-        <h3>I am learning full stack development. Watch this space for my upcoming projects</h3>
+        <div className="app-content">
+          <img src={profileImage} alt="display" />
+          <h3>
+            Hi, I am Sourav Verma, I am 26 years old, living in Lubbock, Texas
+          </h3>
+          <h4>
+            I am learning full stack development. Watch this space for my
+            upcoming projects
+          </h4>
+        </div>
       </header>
     </div>
   );
---
+++ src/components/NavBar.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Navbar, Icon, NavItem } from "react-materialize";
+import "./NavBar.css";
+
+export const NavBar = () => {
+  return (
+    <>
+      <Navbar
+        className="nav-bar"
+        alignLinks="right"
+        brand={
+          <a className="brand-logo" href="">
+            My Portfolio
+          </a>
+        }
+        id="mobile-nav"
+        menuIcon={<Icon>menu</Icon>}
+        options={{
+          draggable: true,
+          edge: "left",
+          inDuration: 250,
+          onCloseEnd: null,
+          onCloseStart: null,
+          onOpenEnd: null,
+          onOpenStart: null,
+          outDuration: 200,
+          preventScrolling: true,
+        }}
+      >
+        <NavItem href="">Resume</NavItem>
+        <NavItem href="">Projects</NavItem>
+        <NavItem>Contact</NavItem>
+      </Navbar>
+    </>
+  );
+};
+
+export default NavBar;
